f8950aab28b7ef4d5141021359f11f6d8dcafc48
yandavida <yandavida@gmail.com>
2025-12-14 11:13:57 +0200

    V2-14.1.1: Add opportunity space models

---
+++ core/arbitrage/opportunity_space/__init__.py
@@ -0,0 +1,8 @@
+from .models import CanonicalKey, EconomicsBreakdown, ExecutionOption, Provenance
+
+__all__ = [
+    "CanonicalKey",
+    "EconomicsBreakdown",
+    "ExecutionOption",
+    "Provenance",
+]
